9a5e0e56d126294786d326b71c2405d4083e7489
Abner Castro <abnerfcastro@gmail.com>
2021-08-04 20:04:33 -0300

    Implement step-2 of the presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  const message = "Hello, SA Team!";
-
+function MostBoringApp() {
   const [value, setValue] = React.useState(0);
 
-  const increment = () => {
-    setValue(value + 1);
+  const onClickHandler = ({ target }: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = target as HTMLButtonElement;
+    switch (name) {
+      case "increment":
+        setValue(value + 1);
+        break;
+      case "decrement":
+        setValue(value - 1);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
-    <div className="App">
-      <h1>{message}</h1>
+    <div>
       <h3>The Most Boring App Ever</h3>
-      <button onClick={increment}>Increment</button>
-      <button onClick={() => setValue(value - 1)}>Decrement</button>
+      <button
+        style={{ marginRight: "5px" }}
+        onClick={onClickHandler}
+        name="increment"
+      >
+        Increment
+      </button>
+      <button name="decrement" onClick={onClickHandler}>
+        Decrement
+      </button>
       <div>
         The current value is:
         <code>{value}</code>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  const message = "Hello, SA Team!";
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <MostBoringApp />
     </div>
   );
 }
